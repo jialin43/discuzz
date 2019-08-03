07b007fb1e1b659d52851eb49421fc691bb2dc89
jialin43 <649386797@qq.com>
2019-08-03 20:04:56 +0800

    参数单独提取出来

--- src/main/java/cn/skycer/discuzz/contoller/AuthorizeController.java
+++ src/main/java/cn/skycer/discuzz/contoller/AuthorizeController.java
@@ -4,6 +4,7 @@ import cn.skycer.discuzz.dto.AccessTokenDTO;
 import cn.skycer.discuzz.dto.GithubUser;
 import cn.skycer.discuzz.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,18 +16,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String secretId;
+    @Value("${github.client.redirect_uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("ffca123b355af39865bc");
+        accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_secret("fea75a254556a6c46f36557e8266bcc83dca839e");
+        accessTokenDTO.setClient_secret(secretId);
         String token = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(token);
         System.out.println(githubUser.getName());
         return "index";
     }
+
+
 }
