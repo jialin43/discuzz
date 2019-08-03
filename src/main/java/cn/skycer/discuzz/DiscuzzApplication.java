abc704a982fff84eaa6a5002544374290a85bd10
jialin43 <649386797@qq.com>
2019-08-03 10:09:58 +0800

    init

---
+++ src/main/java/cn/skycer/discuzz/DiscuzzApplication.java
@@ -0,0 +1,13 @@
+package cn.skycer.discuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscuzzApplication.class, args);
+	}
+
+}
